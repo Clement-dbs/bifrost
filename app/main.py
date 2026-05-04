ddcbc4269657a99e9cdba13708b5e7fd09acbd67
clement <clement.duboisdbs@gmail.com>
2026-05-04 16:48:18 +0200

    Setup main program execution

--- app/main.py
+++ app/main.py
@@ -1,16 +1,33 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
-from routers import routers_auth, sources
+# bifrost db
+
+from database.database import init_db
+
+init_db()  
+
+# Api services
+from api.routers import routers_auth
+from api.routers import sources
+
+# Minio services
+from services.services import init_buckets
+
+init_buckets()
 
 app = FastAPI(
     title="Bifrost API",
     version="1.0.0"
 )
 
-@app.get("/")
-def root():
-    return {"message": "Welcome to Bifrost API"}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(routers_auth.app, prefix="/auth")
 app.include_router(sources.app, prefix="/api")
